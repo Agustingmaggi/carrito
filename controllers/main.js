469c91561f1611bcfa36ad12fd1608fcfec3aad6
Agustingmaggi <agustingmaggi@gmail.com>
2023-04-21 20:21:50 -0300

    ultimos cambio antes de probar lo de mercado pago en heroku

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,5 @@
 const {Product} = require('../database/models')
+const axios = require('axios')
 module.exports = {
     home: async (req, res) => {
         try {
@@ -51,5 +52,42 @@ module.exports = {
         } catch (error){
             res.send(error)
         }
+    },
+    //estos metodos son para mercado pago
+    checkout: async (req, res) => {
+        try{
+
+           let response = await axios.post('https://api.mercadopago.com/checkout/preferences',{
+                items: req.session.cart.map(item => {
+                    return {
+                        title: item.name,
+                        quantity: item.quantity,
+                        currency_id: 'ARS',
+                        unit_price: item.price
+                    }
+                }),
+                back_urls: {
+                    success: 'http://localhost:3030/feedback',
+                    failure: 'http://localhost:3030/feedback',
+                    pending: 'http://localhost:3030/feedback'
+                },
+                auto_return: 'approved',
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${process.env.MP_ACCESS_TOKEN || 'TEST-1249931130347559-042118-5f199316521dd8ea2c71dc45cfa1d6a2-173356790'}`,
+                    'Content-Type': 'application/json'
+                }
+                
+        })
+        return res.redirect(response.data.init_point)
+
+        } catch (error) {
+            res.send(error)
+        }
+    },
+    feedback: async (req, res) => {
+        req.session.destroy()
+        res.send(req.query)
     }
+
 }
--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {home,addCart,deleteCart} = require('../controllers/main')
+const {home,addCart,deleteCart,checkout,feedback} = require('../controllers/main')
 
 router.get('/', home)
+router.get('/checkout', feedback)
 
 router.post('/agregar',addCart)
 router.post('/eliminar', deleteCart)
+
+router.post('/checkout', checkout)
 
 module.exports = router
