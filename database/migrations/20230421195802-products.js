d7c789546e881f1f9aa3c9b4c294b6a4d5844092
Agustingmaggi <agustingmaggi@gmail.com>
2023-04-21 17:27:59 -0300

    Sequelize Add Proyect

---
+++ database/migrations/20230421195802-products.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('products')
+  }
+};
